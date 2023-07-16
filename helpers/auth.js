15458da4922de03b5ac97b9f8242d815d8b0e1ef
Daniel Custódio <play.daniel@hotmail.com>
2023-07-16 17:33:02 -0300

    feat: Salvando pensamentos no banco de dados

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,5 +1,6 @@
 module.exports.checkAuth = function (req, res, next) {
   if (!req.session.userid) {
+    console.log('pAssou ayui');
     res.redirect('/login');
     return;
   }
--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const PostController = require('../controllers/PostController');
-const checkAuth = require('../helpers/auth').checkAuth;
+const { checkAuth } = require('../helpers/auth');
 
 router.get('/add', checkAuth, PostController.createPost);
 router.get('/dashboard', checkAuth, PostController.dashboard);
-router.get('/', PostController.showPosts);
+router.get('/', checkAuth, PostController.showPosts);
+router.post('/add', checkAuth, PostController.createPostSave);
 
 module.exports = router;
